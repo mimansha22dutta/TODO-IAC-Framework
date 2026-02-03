56206254a0ca1e42546163a3e8b81644ed68618e
Mimansha Dutta <mimanshaedu@gmail.com>
2026-02-04 04:56:59 +0530

    updated vm module

--- modules/azurerm_virtual_machine/data.tf
+++ modules/azurerm_virtual_machine/data.tf
@@ -4,10 +4,10 @@ data "azurerm_network_interface" "datanic" {
   resource_group_name = each.value.rg_name
 }
 
-data "azurerm_key_vault_secret" "vm_password" {
-  name         = "vm-password"
-  key_vault_id = data.azurerm_key_vault.kv[each.key].id
-}
+# data "azurerm_key_vault_secret" "vm_password" {
+#   name         = "vm-password"
+#   key_vault_id = data.azurerm_key_vault.kv[each.key].id
+# }
 
 # data "azurerm_key_vault" "kv" {
 #   name                = "mykeyvault"
